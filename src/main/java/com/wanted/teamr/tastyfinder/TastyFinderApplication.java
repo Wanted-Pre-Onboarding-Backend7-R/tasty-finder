84c1abf07587f320acf97d52b0aed8c17e36a694
Joonhee Jeong <jeonggoo75@gmail.com>
2023-10-31 17:10:24 +0900

    feat: 프로젝트 초기설정

---
+++ src/main/java/com/wanted/teamr/tastyfinder/TastyFinderApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.teamr.tastyfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/teamr/tastyfinder/TastyFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.teamr.tastyfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
