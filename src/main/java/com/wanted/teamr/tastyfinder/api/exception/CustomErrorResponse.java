f5765d8f23f13c7317b72f70dbff5e956c1f9020
mojh7 <mjh79017901@gmail.com>
2023-11-03 03:57:43 +0900

    feat #9: 예외 처리 관련 클래스 추가

---
+++ src/main/java/com/wanted/teamr/tastyfinder/api/exception/CustomErrorResponse.java
@@ -0,0 +1,16 @@
+package com.wanted.teamr.tastyfinder.api.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomErrorResponse {
+
+    private final String errorCode;
+    private final String message;
+
+    public CustomErrorResponse(ErrorCodeType errorCodeType) {
+        errorCode = errorCodeType.name();
+        message = errorCodeType.getMessage();
+    }
+
+}
---
+++ src/main/java/com/wanted/teamr/tastyfinder/api/exception/ErrorCodeType.java
@@ -0,0 +1,11 @@
+package com.wanted.teamr.tastyfinder.api.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCodeType {
+
+    String name();
+    String getMessage();
+    HttpStatus getHttpStatus();
+
+}
