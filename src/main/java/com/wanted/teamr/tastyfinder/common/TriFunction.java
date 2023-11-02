f8b4bf67e8966b87f1cdff23895f0bc52517ed33
mojh7 <mjh79017901@gmail.com>
2023-11-03 05:41:23 +0900

    feat #9: 맛집 목록 거리 순, 평점 순 정렬 조회 구현

---
+++ src/main/java/com/wanted/teamr/tastyfinder/common/TriFunction.java
@@ -0,0 +1,18 @@
+package com.wanted.teamr.tastyfinder.common;
+
+import java.util.function.BiFunction;
+
+/**
+ * 파라미터 3개 받는 함수형 인터페이스
+ * <p>
+ * 기본으로 제공된 함수형 인터페이스 중 파라미터 1, 2개만 받는 것만 존재
+ * <p>
+ * {@link BiFunction} 참고
+ */
+@FunctionalInterface
+public interface TriFunction<T, U, V, R> {
+
+    R apply(T t, U u, V v);
+
+}
+
