8160dd2218b446b738ae3543ca146af2a44d3874
Joonhee Jeong <jeonggoo75@gmail.com>
2023-11-06 06:21:47 +0000

    Revert "feat #15: 음식점 데이터 파이프라이닝"

--- src/main/java/com/wanted/teamr/tastyfinder/datapipelining/repository/MatzipRawRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wanted.teamr.tastyfinder.datapipelining.repository;
-
-import com.wanted.teamr.tastyfinder.datapipelining.domain.MatzipRaw;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatzipRawRepository extends JpaRepository<MatzipRaw, Long>, CustomMatzipRawRepository {
-}
